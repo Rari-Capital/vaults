977ba0e1730fc7ee408281aeaa616d2f6c669fe2
t11s <greenfeatherhelp@gmail.com>
2021-06-22 00:07:55 -0700

    👷‍♂️ Fix prettier

--- src/Vaults.sol
+++ src/Vaults.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.7.6;
 
-contract Vaults {
-}
+contract Vaults {}
--- src/Vaults.t.sol
+++ src/Vaults.t.sol
@@ -6,17 +6,18 @@ import "ds-test/test.sol";
 import "./Vaults.sol";
 
 contract VaultsTest is DSTest {
-    Vaults vaults;
+  Vaults vaults;
 
-    function setUp() public {
-        vaults = new Vaults();
-    }
+  function setUp() public {
+    vaults = new Vaults();
+  }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
-    }
+  function testFail_basic_sanity() public {
+    emit log("BRO HELP");
+    assertTrue(false);
+  }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
-    }
+  function test_basic_sanity() public {
+    assertTrue(true);
+  }
 }
