a350f70243a276ec356d91b50facfad56456d2e3
t11s <greenfeatherhelp@gmail.com>
2021-06-26 14:36:11 -0700

    👷‍♂️ Prettier Config

--- src/libraries/StringConcat.sol
+++ src/libraries/StringConcat.sol
@@ -5,21 +5,17 @@ pragma solidity 0.8.6;
 /// @author TransmissionsDev
 /// @dev Uses abi.encodePacked, be aware of SWC-133.
 library StringConcat {
-  /// @notice Concatenates arg1, arg2, and arg3 in that order.
-  function concat(
-    string memory arg1,
-    string memory arg2,
-    string memory arg3
-  ) internal pure returns (string memory) {
-    return string(abi.encodePacked(arg1, arg2, arg3));
-  }
+    /// @notice Concatenates arg1, arg2, and arg3 in that order.
+    function concat(
+        string memory arg1,
+        string memory arg2,
+        string memory arg3
+    ) internal pure returns (string memory) {
+        return string(abi.encodePacked(arg1, arg2, arg3));
+    }
 
-  /// @notice Concatenates arg1, arg2 in that order.
-  function concat(string memory arg1, string memory arg2)
-    internal
-    pure
-    returns (string memory)
-  {
-    return string(abi.encodePacked(arg1, arg2));
-  }
+    /// @notice Concatenates arg1, arg2 in that order.
+    function concat(string memory arg1, string memory arg2) internal pure returns (string memory) {
+        return string(abi.encodePacked(arg1, arg2));
+    }
 }
