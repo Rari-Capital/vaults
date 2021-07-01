4a6424963b9f0b2bbf65f3488b4b45ecd925b256
t11s <greenfeatherhelp@gmail.com>
2021-07-01 14:48:10 -0700

    👷‍♂️ Switch to OZ ERC20

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {ERC20} from "./external/ERC20.sol";
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {CErc20} from "./external/CErc20.sol";
 import {StringConcat} from "./libraries/StringConcat.sol";
 
--- src/VaultFactory.sol
+++ src/VaultFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {ERC20} from "./external/ERC20.sol";
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {Vault} from "./Vault.sol";
 
 /// @title VaultFactory
