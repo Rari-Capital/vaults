30ef5e681e63ee9f5d8763a64d8061b6776e8901
JetDeveloping <jetjadeja@gmail.com>
2021-07-18 13:21:24 -0700

    ✅ Created MockCERC20 contract for tests

---
+++ src/tests/mocks/MockCERC20.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.6;
+
+import "../../external/ERC20.sol";
+
+contract MockCERC20 is ERC20("Mock Token", "MOCK", 18) {
+    ERC20 underlying;
+
+    constructor(ERC20 _underlying) {
+        underlying = _underlying;
+    }
+
+    function mint(uint256 amount) external {
+        // Transfer underlying tokens to the cToken.
+        underlying.transferFrom(msg.sender, address(this), amount);
+
+        // Mint cTokens.
+        _mint(msg.sender, amount);
+    }
+
+    function balanceOfUnderlying() external view returns (uint256) {
+        return underlying.balanceOf(address(this));
+    }
+}
