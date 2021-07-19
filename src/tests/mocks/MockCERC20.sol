e6722c6c5e805ddd7f81d4c431f0d164ddc49a62
t11s <greenfeatherhelp@gmail.com>
2021-07-19 10:46:23 -0700

    ♻️ Clean up MockCERC20

--- src/tests/mocks/MockCERC20.sol
+++ src/tests/mocks/MockCERC20.sol
@@ -3,29 +3,28 @@ pragma solidity 0.8.6;
 
 import "../../external/ERC20.sol";
 
-contract MockCERC20 is ERC20("Mock Token", "MOCK", 18) {
-    ERC20 underlying;
+contract MockCERC20 is ERC20("Mock CERC20", "MCERC20", 18) {
+    ERC20 immutable underlying;
 
     constructor(ERC20 _underlying) {
         underlying = _underlying;
     }
 
     function mint(uint256 amount) external returns (uint256) {
-        // Transfer underlying tokens to the cToken.
         underlying.transferFrom(msg.sender, address(this), amount);
-        // Mint cTokens.
+
         _mint(msg.sender, amount);
 
-        return amount;
+        // TODO: We should prolly return actual error codes and not revert as per CERC20 spec
+        return 0;
     }
 
     function redeem(uint256 redeemTokens) external returns (uint256) {
         underlying.transfer(msg.sender, redeemTokens);
 
         _burn(msg.sender, redeemTokens);
-    }
 
-    function balanceOfUnderlying() external view returns (uint256) {
-        return underlying.balanceOf(address(this));
+        // TODO: We should prolly return actual error codes and not revert as per CERC20 spec
+        return 0;
     }
 }
