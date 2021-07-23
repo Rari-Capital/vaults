e6d36a305323a101a9fdfae0597c462675584e2f
JetDeveloping <jetjadeja@gmail.com>
2021-07-23 15:01:39 -0700

    💡 Added isCEther method to cToken interface

--- src/external/CErc20.sol
+++ src/external/CErc20.sol
@@ -26,9 +26,9 @@ interface CErc20 {
 
     // function totalFuseFees() external view returns (uint256);
 
-    // function isCToken() external view returns (bool);
+    function isCEther() external view returns (bool);
 
-    // function isCEther() external view returns (bool);
+    function underlying() external view returns (address);
 
     function balanceOf(address owner) external view returns (uint256);
 
@@ -51,4 +51,7 @@ interface CErc20 {
     function redeemUnderlying(uint256 redeemAmount) external returns (uint256);
 
     function mint(uint256 mintAmount) external returns (uint256);
+
+    // CETH mint method
+    function mint() external payable returns (uint256);
 }
