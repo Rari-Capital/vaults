3a7ac69387263e3c9a946f4fe08b17e20bf8034f
JetDeveloping <jetjadeja@gmail.com>
2021-08-03 19:06:35 -0600

    ✅ Added harvest profit calculation test

--- src/tests/Vault.t.sol
+++ src/tests/Vault.t.sol
@@ -175,4 +175,51 @@ contract VaultsTest is DSTestPlus {
         hevm.roll(block.number + vault.minimumHarvestDelay());
         assertGt(vault.exchangeRateCurrent(), 1e18);
     }
+
+    function test_harvest_profits_are_correctly_calculated() public {
+        uint256 amount = 1e18;
+
+        if (amount > (type(uint256).max / 1e37) || amount < 40) return;
+
+        // Deposit into the vault.
+        underlying.mint(address(this), amount);
+        underlying.approve(address(vault), amount);
+        vault.deposit(amount);
+
+        // Set the block number to 1.
+        // If the current block number is 1, the vault will act unexpectedly.
+        hevm.roll(1);
+
+        // Allocate the deposited tokens to various cToken contracts.
+        for (uint256 i = 0; i < 10; i++) {
+            // Deploy a new mock cToken contract and add it to the withdrawQueue.
+            CErc20 mockCErc20 = CErc20(address(new MockCERC20(underlying)));
+            withdrawQueue.push(mockCErc20);
+
+            // Deposit 10% of the total supply into the vault.
+            // This ensure that by the end of the loop, 100% of the vault balance is deposited into the cTokens contracts.
+            vault.enterPool(mockCErc20, amount / 10);
+
+            // Transfer tokens to the cToken contract to simulate earned interest.
+            // This simulates a 50% increase.
+            underlying.mint(address(this), amount / 20);
+            underlying.transfer(address(mockCErc20), amount / 20);
+        }
+
+        // Set the withdrawalQueue to the token addresses.
+        vault.setWithdrawalQueue(withdrawQueue);
+
+        // Trigger a harvest.
+        vault.harvest();
+
+        // Set the block number to the next block.
+        hevm.roll(block.number + vault.minimumHarvestDelay());
+
+        emit log_uint(vault.exchangeRateCurrent());
+        assertEq(vault.exchangeRateCurrent(), 1.5e18);
+    }
+
+    function test_harvest_fees_are_correctly_calculated(uint256 amount) public {}
+
+    function test_harvest_fees_mint_and_send_tokens_to_the_right_address(uint256 amount) public {}
 }
