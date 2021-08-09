4a353d99a31275723ed7b76967b21832d59b5063
Jet Jadeja <jetjadeja@gmail.com>
2021-08-09 16:40:48 -0700

    🔇 Removed logs from the harvest function

--- src/Vault.sol
+++ src/Vault.sol
@@ -289,17 +289,9 @@ contract Vault is ERC20, DSTestPlus {
     /// This updates the vault's balance in the cToken contracts,
     /// take fees, and update the float.
     function harvest() external {
-        emit log_named_uint("Total Free Underlying", calculateTotalFreeUnderlying());
-        emit log_named_uint("Total Deposited", totalDeposited);
-
-        emit log("");
-
         // Calculate an updated float value based on the amount of profit during the last harvest.
         uint256 updatedFloat = (totalDeposited * targetFloatPercent) / 1e18;
         if (updatedFloat > getFloat()) pullIntoFloat(updatedFloat - getFloat());
-
-        emit log_named_uint("Total Free Underlying", calculateTotalFreeUnderlying());
-        emit log_named_uint("Total Deposited", totalDeposited);
 
         // Transfer fvTokens (representing fees) to the fee holder
         uint256 _fee = harvestFee;
