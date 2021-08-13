1859d25be15ed35755471b32826c1474643a41f9
Jet Jadeja <jetjadeja@gmail.com>
2021-08-13 15:36:41 -0700

    ✅ Added a mockCEth contract

---
+++ src/tests/mocks/MockCETH.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.6;
+
+import {ERC20} from "solmate/erc20/ERC20.sol";
+
+contract MockCERC20 is ERC20("Mock CETH", "MCETH", 18) {
+    function mint() external payable returns (uint256) {
+        _mint(msg.sender, msg.value);
+
+        // TODO: We should prolly return actual error codes and not revert as per CERC20 spec
+        return 0;
+    }
+
+    function redeem(uint256 redeemTokens) external returns (uint256) {
+        payable(msg.sender).transfer(redeemTokens);
+        _burn(msg.sender, redeemTokens);
+
+        // TODO: We should prolly return actual error codes and not revert as per CERC20 spec
+        return 0;
+    }
+
+    function redeemUnderlying(uint256 redeemTokens) external returns (uint256) {
+        payable(msg.sender).transfer(redeemTokens);
+        _burn(msg.sender, redeemTokens);
+
+        // TODO: We should prolly return actual error codes and not revert as per CERC20 spec
+        return 0;
+    }
+
+    function balanceOfUnderlying(address) external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function isCEther() external pure returns (bool) {
+        return true;
+    }
+}
