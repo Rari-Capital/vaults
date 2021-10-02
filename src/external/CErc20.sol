9153aa5a082cb80aec03f41369284ab0d205fe3b
t11s <greenfeatherhelp@gmail.com>
2021-10-01 22:40:10 -0700

    ♻️ CERC20 cleanup

--- src/external/CErc20.sol
+++ src/external/CErc20.sol
@@ -2,6 +2,23 @@
 pragma solidity 0.8.6;
 
 interface CErc20 {
+    function isCEther() external view returns (bool);
+
+    function balanceOf(address owner) external view returns (uint256);
+
+    function balanceOfUnderlying(address owner) external returns (uint256);
+
+    function redeem(uint256 redeemTokens) external returns (uint256);
+
+    function redeemUnderlying(uint256 redeemAmount) external returns (uint256);
+
+    function mint(uint256 mintAmount) external returns (uint256);
+
+    function mint() external payable returns (uint256);
+
+    // TODO: Maybe add underlying so we can check when entering a pool that the cToken accepts the right asset?
+    // function underlying() external view returns (address);
+
     // function admin() external view returns (address);
 
     // function adminHasRights() external view returns (bool);
@@ -26,14 +43,6 @@ interface CErc20 {
 
     // function totalFuseFees() external view returns (uint256);
 
-    function isCEther() external view returns (bool);
-
-    // function underlying() external view returns (address);
-
-    function balanceOf(address owner) external view returns (uint256);
-
-    function balanceOfUnderlying(address owner) external returns (uint256);
-
     // function borrowRatePerBlock() external view returns (uint256);
 
     // function supplyRatePerBlock() external view returns (uint256);
@@ -45,13 +54,4 @@ interface CErc20 {
     // function exchangeRateStored() external view returns (uint256);
 
     // function getCash() external view returns (uint256);
-
-    function redeem(uint256 redeemTokens) external returns (uint256);
-
-    function redeemUnderlying(uint256 redeemAmount) external returns (uint256);
-
-    function mint(uint256 mintAmount) external returns (uint256);
-
-    // CETH mint method
-    function mint() external payable returns (uint256);
 }
