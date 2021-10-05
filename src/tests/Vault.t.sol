8725bbe2b0bc2fc67c65203e04cdbf2f0555012c
t11s <greenfeatherhelp@gmail.com>
2021-10-04 21:08:57 -0700

    🚧 Tests

--- src/tests/Vault.t.sol
+++ src/tests/Vault.t.sol
@@ -6,6 +6,7 @@ import {MockERC20} from "solmate/tests/utils/MockERC20.sol";
 import {DSTestPlus} from "./utils/DSTestPlus.sol";
 
 import {Vault} from "../Vault.sol";
+import {VaultFactory} from "../VaultFactory.sol";
 
 contract VaultsTest is DSTestPlus {
     Vault vault;
@@ -13,6 +14,24 @@ contract VaultsTest is DSTestPlus {
 
     function setUp() public {
         underlying = new MockERC20("Mock Token", "TKN", 18);
-        vault = new Vault(underlying);
+        vault = new VaultFactory().deployVault(underlying);
+    }
+
+    function testAtomicDepositWithdraw() public {
+        underlying.mint(address(this), 1e18);
+
+        uint256 preDepositBal = underlying.balanceOf(address(this));
+
+        underlying.approve(address(vault), 1e18);
+
+        vault.deposit(1e18);
+
+        assertEq(underlying.balanceOf(address(this)), preDepositBal - 1e18);
+        assertEq(vault.underlyingBalanceOf(address(this)), 1e18);
+
+        vault.withdraw(1e18);
+
+        assertEq(underlying.balanceOf(address(this)), preDepositBal);
+        assertEq(vault.underlyingBalanceOf(address(this)), 0);
     }
 }
