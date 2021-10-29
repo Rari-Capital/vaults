2acb25cd28b35205f7740f451f368876dfa3d266
t11s <greenfeatherhelp@gmail.com>
2021-10-28 19:32:31 -0700

    ♻️ Cleanup MockStrategy

--- src/test/mocks/MockStrategy.sol
+++ src/test/mocks/MockStrategy.sol
@@ -7,7 +7,7 @@ import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
 import {ERC20Strategy} from "../../interfaces/Strategy.sol";
 
-contract MockStrategy is ERC20("Rari Vault Mock Strategy", "rvsMOCK", 18), ERC20Strategy {
+contract MockStrategy is ERC20("Mock cToken Strategy", "cMOCK", 18), ERC20Strategy {
     using SafeERC20 for ERC20;
     using FixedPointMathLib for uint256;
 
@@ -30,27 +30,23 @@ contract MockStrategy is ERC20("Rari Vault Mock Strategy", "rvsMOCK", 18), ERC20
     }
 
     function mint(uint256 underlyingAmount) external override returns (uint256) {
-        // Convert UNDERLYING tokens to cTokens and mint them.
         _mint(msg.sender, underlyingAmount.fdiv(exchangeRate(), BASE_UNIT));
 
-        // Transfer in UNDERLYING tokens from the sender.
         UNDERLYING.safeTransferFrom(msg.sender, address(this), underlyingAmount);
 
         return 0;
     }
 
     function redeemUnderlying(uint256 underlyingAmount) external override returns (uint256) {
-        // Convert UNDERLYING tokens to cTokens and then burn them.
         _burn(msg.sender, underlyingAmount.fdiv(exchangeRate(), BASE_UNIT));
 
-        // Transfer UNDERLYING tokens to the caller.
         UNDERLYING.safeTransfer(msg.sender, underlyingAmount);
 
         return 0;
     }
 
-    function balanceOfUnderlying(address account) external view override returns (uint256) {
-        return balanceOf[account].fmul(exchangeRate(), BASE_UNIT);
+    function balanceOfUnderlying(address user) external view override returns (uint256) {
+        return balanceOf[user].fmul(exchangeRate(), BASE_UNIT);
     }
 
     /*///////////////////////////////////////////////////////////////
@@ -62,11 +58,11 @@ contract MockStrategy is ERC20("Rari Vault Mock Strategy", "rvsMOCK", 18), ERC20
     uint256 internal immutable BASE_UNIT;
 
     function exchangeRate() internal view returns (uint256) {
-        // If there are no cTokens in circulation, return an exchange rate of 1:1.
-        if (totalSupply == 0) return BASE_UNIT;
+        uint256 cTokenSupply = totalSupply;
 
-        // TODO: Optimize double SLOAD of totalSupply here?
-        return UNDERLYING.balanceOf(address(this)).fdiv(totalSupply, BASE_UNIT);
+        if (cTokenSupply == 0) return BASE_UNIT;
+
+        return UNDERLYING.balanceOf(address(this)).fdiv(cTokenSupply, BASE_UNIT);
     }
 
     /*///////////////////////////////////////////////////////////////
@@ -74,6 +70,6 @@ contract MockStrategy is ERC20("Rari Vault Mock Strategy", "rvsMOCK", 18), ERC20
     //////////////////////////////////////////////////////////////*/
 
     function simulateLoss(uint256 underlyingAmount) external {
-        UNDERLYING.safeTransfer(0x000000000000000000000000000000000000dEaD, underlyingAmount);
+        UNDERLYING.safeTransfer(address(0xDEAD), underlyingAmount);
     }
 }
