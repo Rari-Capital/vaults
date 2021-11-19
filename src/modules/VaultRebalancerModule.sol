eb8e1dc30e923efa73399059f9ac5af6402379fa
JetDeveloping <jetjadeja@gmail.com>
2021-11-19 00:07:15 -0800

    ✨ Changed to a single action system

--- src/modules/VaultRebalancerModule.sol
+++ src/modules/VaultRebalancerModule.sol
@@ -7,61 +7,13 @@ import {Vault} from "../Vault.sol";
 /// @author Transmissions11 and JetJadeja
 /// @notice Module that automatically rebalances the Vault
 contract VaultRebalancerModule {
-    struct Action {
-        Strategy strategy;
-        uint256 amount;
-    }
-
     function rebalance(
         Vault vault,
-        Action[] memory strategiesToWithdrawFrom,
-        Action[] memory strategiesToDepositInto
+        Strategy withdrawalContract,
+        Strategy depositContract,
+        uint256 amount
     ) external {
-        uint256 totalWithdraw;
-        uint256 totalDeposit;
-        uint256 vaultBaseUnit = vault.BASE_UNIT();
-
-        for (uint256 i = 0; i < strategiesToWithdrawFrom.length; i++) {
-            totalWithdraw += strategiesToWithdrawFrom[i].amount;
-            vault.withdrawFromStrategy(strategiesToWithdrawFrom[i].strategy, strategiesToWithdrawFrom[i].amount);
-        }
-
-        uint256 interestInWithdrawalPools = calculateWeightedAverage(
-            strategiesToWithdrawFrom,
-            totalWithdraw,
-            vaultBaseUnit
-        );
-
-        for (uint256 i = 0; i < strategiesToDepositInto.length; i++) {
-            totalDeposit += strategiesToDepositInto[i].amount;
-            vault.depositIntoStrategy(strategiesToDepositInto[i].strategy, strategiesToDepositInto[i].amount);
-        }
-
-        uint256 interestInDepositPools = calculateWeightedAverage(strategiesToDepositInto, totalDeposit, vaultBaseUnit);
-
-        require(interestInWithdrawalPools < interestInDepositPools, "");
-    }
-
-    function calculateWeightedAverage(
-        Action[] memory strategies,
-        uint256 total,
-        uint256 baseUnit
-    ) internal returns (uint256) {
-        uint256 weightedTotal;
-        uint256 sumOfWeights;
-
-        for (uint256 i = 0; i < strategies.length; i++) {
-            // Weight of the strategy, scaled by 10^decimals.
-            uint256 weight = (strategies[i].amount * baseUnit) / total;
-
-            // Add to the sum of the weights.
-            sumOfWeights += weight;
-
-            // Add to the weighted total amount.
-            weightedTotal += weight * strategies[i].strategy.supplyRatePerBlock();
-        }
-
-        // Divide the weighted total by the number of strategies.
-        return weightedTotal / sumOfWeights;
+        vault.withdrawFromStrategy(withdrawalContract, amount);
+        vault.depositIntoStrategy(depositContract, amount);
     }
 }
