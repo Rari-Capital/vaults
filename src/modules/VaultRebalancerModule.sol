daefda90025b23cd3abad4261e67c1feeab1a876
JetDeveloping <jetjadeja@gmail.com>
2021-11-23 10:37:06 -1000

    📝 Update rebalance module documentation

--- src/modules/VaultRebalancerModule.sol
+++ src/modules/VaultRebalancerModule.sol
@@ -13,15 +13,20 @@ contract VaultRebalancerModule {
         Strategy depositContract,
         uint256 amount
     ) external {
+        // Calculate what percentage of the Vault's totalHoldings are being transfered
         uint256 shareOfVault = (amount * vault.BASE_UNIT()) / vault.totalHoldings();
 
+        // Ensure that less than 15% of the Vault is being transferred.
         require(shareOfVault < ((3 * vault.totalHoldings()) / 20));
 
+        // Store the supply rate of the strategy being withdrew from.
         uint256 withdrawalSupplyRate = withdrawalContract.supplyRatePerBlock();
 
+        // Transfer the tokens
         vault.withdrawFromStrategy(withdrawalContract, amount);
         vault.depositIntoStrategy(depositContract, amount);
 
+        // Ensure that the supply rate of the new strategy is larger than the supply rate of the old strategy
         require(depositContract.supplyRatePerBlock() > withdrawalSupplyRate, "RATE_MUST_INCREASE");
     }
 }
