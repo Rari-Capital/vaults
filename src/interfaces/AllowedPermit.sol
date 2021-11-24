3153dbe715441f1fe877f45472e9a633e4cf7b1b
t11s <greenfeatherhelp@gmail.com>
2021-11-23 19:09:34 -0800

    📝 Comment fixes

--- src/interfaces/AllowedPermit.sol
+++ src/interfaces/AllowedPermit.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.10;
 
-interface AllowedPermit {
-    /// @dev Non-standard permit interface used by DAI.
+import {ERC20} from "solmate/tokens/ERC20.sol";
+
+/// @notice Minimal interface for tokens using DAI's non-standard permit interface.
+/// @author Modified from Uniswap (https://github.com/Uniswap/v3-periphery/blob/main/contracts/interfaces/external/IERC20PermitAllowed.sol)
+abstract contract AllowedPermit is ERC20 {
     /// @param holder The address of the token owner.
     /// @param spender The address of the token spender.
     /// @param nonce The owner's nonce, increases at each call to permit.
@@ -20,5 +23,5 @@ interface AllowedPermit {
         uint8 v,
         bytes32 r,
         bytes32 s
-    ) external;
+    ) external virtual;
 }
--- src/interfaces/Strategy.sol
+++ src/interfaces/Strategy.sol
@@ -3,20 +3,44 @@ pragma solidity 0.8.10;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
+/// @notice Minimal interface for Vault compatible strategies.
+/// @dev Designed for out of the box compatibility with Fuse cTokens.
+/// @dev Like cTokens, strategies must be transferrable ERC20s.
 abstract contract Strategy is ERC20 {
+    /// @notice Returns whether the strategy accepts ETH or an ERC20.
+    /// @return True if the strategy accepts ETH, false otherwise.
+    /// @dev Only present in Fuse cTokens, not Compound cTokens.
     function isCEther() external view virtual returns (bool);
 
+    /// @notice Withdraws a specific amount of underlying tokens from the strategy.
+    /// @param amount The amount of underlying tokens to withdraw.
+    /// @return An error code, or 0 if the withdrawal was successful.
     function redeemUnderlying(uint256 amount) external virtual returns (uint256);
 
+    /// @notice Returns a user's strategy balance in underlying tokens.
+    /// @param user The user to get the underlying balance of.
+    /// @return The user's strategy balance in underlying tokens.
+    /// @dev May mutate the state of the strategy by accruing interest.
     function balanceOfUnderlying(address user) external virtual returns (uint256);
 }
 
+/// @notice Minimal interface for Vault strategies that accept ERC20s.
+/// @dev Designed for out of the box compatibility with Fuse cERC20s.
 abstract contract ERC20Strategy is Strategy {
+    /// @notice Returns the underlying ERC20 token the strategy accepts.
+    /// @return The underlying ERC20 token the strategy accepts.
     function underlying() external view virtual returns (ERC20);
 
+    /// @notice Deposit a specific amount of underlying tokens into the strategy.
+    /// @param amount The amount of underlying tokens to deposit.
+    /// @return An error code, or 0 if the deposit was successful.
     function mint(uint256 amount) external virtual returns (uint256);
 }
 
+/// @notice Minimal interface for Vault strategies that accept ETH.
+/// @dev Designed for out of the box compatibility with Fuse cEther.
 abstract contract ETHStrategy is Strategy {
+    /// @notice Deposit a specific amount of ETH into the strategy.
+    /// @dev The amount of ETH is specified via msg.value. Reverts on error.
     function mint() external payable virtual;
 }
