c2458cd8932afc106390d70cb8c429d965db922e
JetDeveloping <jetjadeja@gmail.com>
2022-03-05 20:36:58 -0800

    🎨 Update import structure

--- src/Vault.sol
+++ src/Vault.sol
@@ -2,12 +2,13 @@
 pragma solidity 0.8.10;
 
 import {Auth} from "solmate/auth/Auth.sol";
-import {WETH} from "solmate/tokens/WETH.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
+
 import {SafeCastLib} from "solmate/utils/SafeCastLib.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
+import {WETH} from "solmate/tokens/WETH.sol";
 import {Strategy, ERC20Strategy, ETHStrategy} from "./interfaces/Strategy.sol";
 
 /// @title Rari Vault (rvToken)
